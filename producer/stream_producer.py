082da817f0ac5f213ca1f281fbad8a815f0680dc
kavyamunnangi1 <kavyamunnangi5@gmail.com>
2026-07-23 03:24:11 +0530

    Add stream producer for restaurant orders
    Initial implementation of a stream producer for restaurant orders.

---
+++ producer/stream_producer.py
@@ -0,0 +1,8 @@
+"""
+Stream Producer
+
+This script will replay restaurant order records as a simulated live stream
+and send them to AWS Kinesis Data Streams.
+"""
+
+print("Restaurant order stream producer placeholder")
